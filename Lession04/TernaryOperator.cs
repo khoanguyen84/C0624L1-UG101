9d76cdfea3fcc387a538d71f6f7a568c4f2b07c1
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-06-26 21:39:05 +0700

    ternary operator

--- Lession04/TernaryOperator.cs
+++ Lession04/TernaryOperator.cs
@@ -3,9 +3,9 @@ class TernaryOperator
 {
     static void Main(string[] args)
     {
-        bool gender = true;
-        bool married = true;
-        string firstName = "Khoa";
+        // bool gender = true;
+        // bool married = true;
+        // string firstName = "Khoa";
         // gender = false => Ms. Bình
         // gender = true => Mr. Bình
         // gender = false => married = false => Ms. Bình
@@ -33,6 +33,12 @@ class TernaryOperator
         // Console.Write(gender ? $"Mr. {firstName}": 
         //                     married ? $"Mrs. {firstName}" : $"Ms. {firstName}");
 
-        Console.Write($"{(gender ? "Mr" : married ? "Mrs": "Ms")}. {firstName}");
+        // Console.Write($"{(gender ? "Mr" : married ? "Mrs": "Ms")}. {firstName}");
+        int number_1 = 5;
+        int number_2 = 10;
+        int number_3 = 7;
+        int max = number_1 > number_2 && number_1 > number_3 ? number_1 : 
+                                    number_2 > number_1 && number_2 > number_3 ? number_2 : number_3;   
+        Console.Write($"max = {max}");
     }
 }
