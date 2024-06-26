9137c4be3bd72194459bc6f1b7b29a5e85de03bb
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-06-26 18:57:44 +0700

    C# basic

---
+++ Lession04/Condition.cs
@@ -0,0 +1,61 @@
+using System.Text;
+namespace Lession04;
+class Condition
+{
+    static void Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+        // int age = 17;
+        // if(age >= 18)
+        // {
+        //     Console.WriteLine("Bạn đủ điều kiện để thi tốt nghiệp THPT");
+        // }
+        // bool gender = true;
+        // // if(gender == true) // true == true => true || false == true => false
+        // if(!gender) // true == true => true || false == true => false
+        // {
+        //     Console.WriteLine("Male");
+        // }
+        // else 
+        // {
+        //     Console.WriteLine("Female");
+        // }
+
+        // gender = true => Mr. Khoa
+        // gender = false => married = true => Mrs. Bình
+        // gender = false => married = false => Ms. Bình
+        // bool gender = false;
+        // bool married = false;
+        // string name = "Bình";
+        // if (gender)
+        // {
+        //     Console.WriteLine($"Mr.{name}");
+        // }
+        // else if (married)
+        //     {
+        //         Console.WriteLine($"Mrs.{name}");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine($"Ms.{name}");
+        //     }
+        
+        // if (gender)
+        // {
+        //     Console.WriteLine($"Mr.{name}");
+        // }
+        // else
+        // {
+        //     if (married)
+        //     {
+        //         Console.WriteLine($"Mrs.{name}");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine($"Ms.{name}");
+        //     }
+        // }
+
+    }
+}
---
+++ Lession04/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+namespace Lession04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            // Console.WriteLine("Xin chào anh em");
+            // Console.WriteLine("Lớp Game Unity");
+            // Console.Write("Enter your name: ");
+            // Console.ReadLine();
+            //Khai báo biến => read/write
+            // double age = 185;
+            // int Age = 19;
+
+            // string _fullname = "Quân Lê";
+            //---
+            // _fullname = "Khoa Nguyễn";
+            // _fullname = "Chánh Trần";
+
+            //khai báo hằng => readonly
+            // const double pi = 3.14;  //const = constant
+
+
+            //khai báo biến bằng từ khóa var = variable
+            // var score = 8;
+            // Console.WriteLine(score.GetType()); //GetType() => trả về kiểu dữ liệu
+
+
+            // lưu trữ dữ liệu => xử lý dữ liệu => toán tử
+            /*
+                ctrl + / => comment/uncomment
+                1. Toán tử số học/toán học: +, -, *, /, % => kết quả là một giá trị số
+                2. Toán so sánh: >, >=, <, <=, ==  => kết quả là giá trị bool (true/false)
+                3. Toán tử logic: && (and), || (or), ! (not) => kết hợp nhiều biểu thức logic => kết quả trả về giá trị bool
+                4. Toán tử cộng chuỗi: + => kết quả trả về là 1 chuỗi
+            */
+            // int soDu = 10 % 4;
+            // string firstname = "Khoa";
+            // string lastname = "Nguyễn";
+            // string fullname = firstname + " " + lastname;
+            // string fullname = string.Format("{0} {1}", firstname, lastname);
+            // string template
+            // string fullname = $"{firstname} {lastname}";
+            // string statement = $"{5 * 2} + {8 * 4} = {10 + 32}";
+            // Console.WriteLine(statement);
+
+            // Console.Write("Enter you age: ");
+            // string age = Console.ReadLine();
+            // Console.WriteLine($"You entered: {age}");
+            // Console.Write("Enter number 1 = ");
+            // int number_1 = int.Parse(Console.ReadLine());
+            // Console.Write("Enter number 2 = ");
+            // int number_2 = int.Parse(Console.ReadLine());
+            // Console.WriteLine($"{number_1} + {number_2} = {number_1 + number_2}");
+
+            // Convert from datatype to datatype
+        }
+    }
+}
+
+
