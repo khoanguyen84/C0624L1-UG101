1eb8ce368d95189ea050035ccdd63ad48c30de78
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-06-28 19:33:23 +0700

    loop statement

---
+++ Loops/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            b1: số lần lặp = 10
+            b2: tại mỗi lần lặp => làm cái gì
+            b2: bắt đầu từ đâu? (optional)
+        */
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        // Console.WriteLine("Hello everyone!");
+        for (byte start = 100; start < 109; start += 2)
+        {
+            Console.WriteLine("Hello everyone!");
+        }
+
+        // for(byte end = 105; end > 100; end--)
+        // {
+        //     Console.WriteLine("How are you tonight");
+        // }
+        // int number = 10;
+        // number = number + 5;
+        // number += 5; //~ number = number + 5
+        // number = number + 1;
+        // number += 1;
+        // number++; // ~ number += 1;
+    }
+}
