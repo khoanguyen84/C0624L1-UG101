cbb84b0fdb27c9f2dab8c281a3e70c804f8a5608
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2024-07-01 19:14:36 +0700

    nested loop

--- Loops/NestedLoop.cs
+++ Loops/NestedLoop.cs
@@ -7,14 +7,17 @@ class NestedLoop
         // row = 10
         // element = 15 sử dụng *
         int rows = 10, elements = 15;
-        for (int row = 1; row <= rows; row++)
+        for (int row = 1; row <= rows; row++) //n
         {
             Console.Write($"{row}\t"); //\t = tab = 1.27cm
-            for (int element = 1; element <= elements; element++)
+            for (int element = 1; element <= elements; element++) //n
             {
                 Console.Write("*");
             }
             Console.Write("\n"); //\t = new line 
         }
+        // Giả sử dữ liệu đủ lớn
+        // Big O = Độ phức tạp của thuật toán = chi phí để thực thi thuật toán => đơn vị thời gian
+        // O(n^n) O(n^2)
     }
 }
